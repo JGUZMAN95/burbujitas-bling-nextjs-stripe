167a208e554be4462c0abdb7b8c8f7165703c53d
Jocelyn Guzman <jocey.g.95@gmail.com>
2025-08-18 17:32:57 -0700

    adujsments for netlify

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,12 +1,8 @@
 import { createClient } from "next-sanity";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-08-01";
-
-export const sanityClient = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, // ✅ required
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
   useCdn: true,
 });
